92de28375cb6ba894015e7006470bd631a3a60af
61lf0yl3 <54092529+61lf0yl3@users.noreply.github.com>
2022-05-25 22:21:42 -0400

    Time: 1 ms (91.98%), Space: 44.2 MB (71.76%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -0,0 +1,34 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean isColZero = false;
+        for (int r = 0; r < matrix.length; r++) {
+            if (matrix[r][0]==0) {
+                isColZero = true;
+            }
+            for (int c = 1; c < matrix[0].length; c++) {
+               if (matrix[r][c] == 0) {
+                   matrix[r][0] = 0;
+                   matrix[0][c] = 0;
+               }
+           }
+        }
+        
+        for (int r = 1; r < matrix.length; r++) {
+            for (int c = 1; c < matrix[0].length; c++) {
+                if (matrix[r][0] == 0 || matrix[0][c] == 0) {
+                    matrix[r][c] = 0;
+                }
+            }
+        }
+        
+        
+        if (matrix[0][0] == 0) {
+            matrix[0] = new int[matrix[0].length];
+        }
+        if (isColZero) {
+            for (int r = 0; r < matrix.length; r++) {
+                matrix[r][0] = 0;
+            }
+        }
+    }
+}
