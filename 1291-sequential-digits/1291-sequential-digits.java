b7aa609f0e41e57c21a633ee0af900e0a011b1e9
61lf0yl3 <54092529+61lf0yl3@users.noreply.github.com>
2022-06-03 01:26:37 -0400

    Time: 1 ms (48.23%), Space: 41.6 MB (46.14%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.java
+++ 1291-sequential-digits/1291-sequential-digits.java
@@ -1,19 +1,25 @@
-class Solution {
-    public List<Integer> sequentialDigits(int low, int high) {
-        ArrayList<Integer> res = new ArrayList<>();
-        
-        for (int length = 1; length <= 9; length++) {
-            for (int start = 1; start <= 9-length; start++) {
-                int num = 0;
-                for (int i = start; i <= start+length; i++) {
-                    num *= 10;
-                    num += i;
-                }
-                if (low <= num && num <= high) {
-                    res.add(num);
-                }
-            }
-        }
-        return res;
+class Seq {
+  public List<Integer> nums = new ArrayList();
+  Seq() {
+    String sample = "123456789";
+    int n = 10;
+
+    for (int length = 2; length < n; ++length) {
+      for (int start = 0; start < n - length; ++start) {
+        int num = Integer.parseInt(sample.substring(start, start + length));
+        nums.add(num);
+      }
     }
+  }
+}
+
+class Solution {
+  public static Seq s = new Seq();
+  public List<Integer> sequentialDigits(int low, int high) {
+    List<Integer> output = new ArrayList();
+    for (int num : s.nums) {
+      if (num >= low && num <= high) output.add(num);
+    }
+    return output;
+  }
 }
