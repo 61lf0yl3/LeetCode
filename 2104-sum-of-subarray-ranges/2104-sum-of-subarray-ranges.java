591c778d7e2d43999e8d0b7a30843eaffda1dfe3
61lf0yl3 <54092529+61lf0yl3@users.noreply.github.com>
2022-06-06 23:24:44 -0400

    Time: 34 ms (27.61%), Space: 45.3 MB (49.27%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -1,14 +1,28 @@
 class Solution {
-    public long subArrayRanges(int[] nums) {
+    public long subArrayRanges(int[] A) {
+        int n = A.length, j, k;
         long res = 0;
-        for (int i = 0; i < nums.length; i++) {
-            long min = nums[i];
-            long max = nums[i];
-            for (int j = i+1; j < nums.length; j++) {
-                min = Math.min(min, nums[j]);
-                max = Math.max(max, nums[j]);
-                res += max-min;
+        
+        Stack<Integer> s = new Stack<>();
+        for (int i = 0; i <= n; i++) {
+            while (!s.isEmpty() && A[s.peek()] > (i == n ? Integer.MIN_VALUE : A[i])) {
+                j = s.pop();
+                k = s.isEmpty() ? -1 : s.peek();
+                res -= (long)A[j] * (i - j) * (j - k);
+
             }
+            s.push(i);
+        }
+        
+        s.clear();
+        for (int i = 0; i <= n; i++) {
+            while (!s.isEmpty() && A[s.peek()] < (i == n ? Integer.MAX_VALUE : A[i])) {
+                j = s.pop();
+                k = s.isEmpty() ? -1 : s.peek();
+                res += (long)A[j] * (i - j) * (j - k);
+
+            }
+            s.push(i);
         }
         return res;
     }
