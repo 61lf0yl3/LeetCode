48e94edd307f0ccc3ff8be0b27db8d2d535832dd
61lf0yl3 <54092529+61lf0yl3@users.noreply.github.com>
2022-06-09 00:16:22 -0400

    Time: 3 ms (89.38%), Space: 44 MB (52.76%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.java
@@ -1,48 +1,52 @@
 class Solution {
     public int threeSumMulti(int[] A, int target) {
         int MOD = 1_000_000_007;
+
+        // Initializing as long saves us the trouble of
+        // managing count[x] * count[y] * count[z] overflowing later.
         long[] count = new long[101];
-        for (int x: A)
+        int uniq = 0;
+        for (int x: A) {
             count[x]++;
+            if (count[x] == 1)
+                uniq++;
+        }
+
+        int[] keys = new int[uniq];
+        int t = 0;
+        for (int i = 0; i <= 100; ++i)
+            if (count[i] > 0)
+                keys[t++] = i;
 
         long ans = 0;
+        // Now, let's do a 3sum on "keys", for i <= j <= k.
+        // We will use count to add the correct contribution to ans.
 
-        // All different
-        for (int x = 0; x <= 100; ++x)
-            for (int y = x+1; y <= 100; ++y) {
-                int z = target - x - y;
-                if (y < z && z <= 100) {
-                    ans += count[x] * count[y] * count[z];
+        for (int i = 0; i < keys.length; ++i) {
+            int x = keys[i];
+            int T = target - x;
+            int j = i, k = keys.length - 1;
+            while (j <= k) {
+                int y = keys[j], z = keys[k];
+                if (y + z < T) {
+                    j++;
+                } else if (y + z > T) {
+                    k--;
+                } else {  // # x+y+z == T, now calc the size of the contribution
+                    if (i < j && j < k) {
+                        ans += count[x] * count[y] * count[z];
+                    } else if (i == j && j < k) {
+                        ans += count[x] * (count[x] - 1) / 2 * count[z];
+                    } else if (i < j && j == k) {
+                        ans += count[x] * count[y] * (count[y] - 1) / 2;
+                    } else {  // i == j == k
+                        ans += count[x] * (count[x] - 1) * (count[x] - 2) / 6;
+                    }
+
                     ans %= MOD;
+                    j++;
+                    k--;
                 }
-            }
-
-        // x == y != z
-        for (int x = 0; x <= 100; ++x) {
-            int z = target - 2*x;
-            if (x < z && z <= 100) {
-                ans += count[x] * (count[x] - 1) / 2 * count[z];
-                ans %= MOD;
-            }
-        }
-
-        // x != y == z
-        for (int x = 0; x <= 100; ++x) {
-            if (target % 2 == x % 2) {
-                int y = (target - x) / 2;
-                if (x < y && y <= 100) {
-                    ans += count[x] * count[y] * (count[y] - 1) / 2;
-                    ans %= MOD;
-                }
-            }
-        }
-
-        // x == y == z
-        if (target % 3 == 0) {
-            int x = target / 3;
-            if (0 <= x && x <= 100) {
-                ans += count[x] * (count[x] - 1) * (count[x] - 2) / 6;
-                ans %= MOD;
             }
         }
 
