fd450b16e6996a923b4534d3d4bbda21c59360e1
61lf0yl3 <54092529+61lf0yl3@users.noreply.github.com>
2022-07-11 02:03:51 -0400

    Time: 7 ms (79.52%), Space: 42.7 MB (82.79%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -6,15 +6,14 @@ class Solution {
         }
         int left = 0;
         int right = 0;
-        int[] temp = Arrays.copyOf(chars, 26);
         while (right < s2.length()) {
             char c = s2.charAt(right);
-            if (temp[c-'a']==0) {
-                temp[s2.charAt(left)-'a']++;
+            if (chars[c-'a']==0) {
+                chars[s2.charAt(left)-'a']++;
                 left++;
                 continue;
             } else {
-                temp[c-'a']--;
+                chars[c-'a']--;
                 if (right-left+1==s1.length()) {
                     return true;
                 }
