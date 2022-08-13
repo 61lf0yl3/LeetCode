09494cd6038dfdeeb99fee9a4bf9df09efaf6523
61lf0yl3 <54092529+61lf0yl3@users.noreply.github.com>
2022-08-12 23:16:18 -0700

    Time: 4 ms (31.76%), Space: 41.7 MB (78.31%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -2,6 +2,7 @@ class Solution {
     public boolean isValid(String s) {
         
         Stack<Character> stack = new Stack<>();
+        
         for (char c : s.toCharArray()) {
             if (c == '(' || c == '{' || c == '[') {
                 stack.add(c);
