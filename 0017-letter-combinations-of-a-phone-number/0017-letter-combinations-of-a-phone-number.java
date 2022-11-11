d6da0e224019956032af002d2572d9ffacc3feea
61lf0yl3 <54092529+61lf0yl3@users.noreply.github.com>
2022-11-10 22:47:31 -0800

    Time: 1 ms (96.86%), Space: 40.8 MB (96.18%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,30 @@
+class Solution {
+    private Map<Character, String> letters = Map.of(
+        '2', "abc", '3', "def", '4', "ghi", '5', "jkl", 
+        '6', "mno", '7', "pqrs", '8', "tuv", '9', "wxyz");
+    
+    private String digits;
+    private List<String> res;
+    
+    public List<String> letterCombinations(String digits) {
+        this.digits = digits;
+        res = new ArrayList<>();
+        if (digits.length()==0) {
+            return res;
+        }
+        backtrack(0, new StringBuilder());
+        return res;
+    }
+    
+    private void backtrack(int i, StringBuilder comb) {
+        if (i >= digits.length()) {
+            res.add(comb.toString());
+            return; 
+        }
+        for (char c : letters.get(digits.charAt(i)).toCharArray()) {
+            comb.append(c);
+            backtrack(i+1, comb);
+            comb.deleteCharAt(comb.length()-1);
+        }
+    }
+}
