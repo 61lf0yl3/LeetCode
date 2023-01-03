1afd987ca8e9f96f49a04fe23c821a66c3af5f85
61lf0yl3 <54092529+61lf0yl3@users.noreply.github.com>
2023-01-02 17:35:55 -0800

    Time: 3 ms (82.02%), Space: 7.6 MB (55.89%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<int> nums;
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        this->nums = nums;
+        for (int i = 0; i <= nums.size(); i++) {
+            vector<int> comb;
+            backtrack(comb, i, 0);
+        }
+        return res;
+    }
+    
+    void backtrack(vector<int>& comb, int size, int i) {
+        if (size == comb.size()) {
+            res.push_back(comb);
+            return;
+        } else {
+            for (int j = i; j < nums.size(); j++) {
+                if (j == i || nums[j-1] != nums[j]) {
+                    comb.push_back(nums[j]);
+                    backtrack(comb, size, j+1);
+                    comb.pop_back();
+                }
+            }
+        }
+    }
+};
