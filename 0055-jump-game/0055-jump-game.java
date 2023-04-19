5c988cc401f7771983e847e7701497363da330a4
61lf0yl3 <54092529+61lf0yl3@users.noreply.github.com>
2023-04-19 01:13:12 -0700

    Time: 1 ms (99.98%), Space: 43.5 MB (23.37%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,32 +1,14 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        boolean[] dp = new boolean[nums.length];
-        dp[nums.length-1] = true;
-        for (int i = nums.length-2; i >= 0; i--) {
-            for (int j = 1; j <= nums[i]; j++) {
-                if (dp[i+j]) {
-                    dp[i] = true;
-                    break;
-                }
-            }
-        }
-        return dp[0];
-    }
-    
-//     private boolean dfs(int[] nums, int i, Boolean[] memo) {
-//         if (i >= nums.length-1) {
-//             return true;
-//         }
+        int reachable = nums.length-1;
         
-//         if (memo[i] == null) {
-//             for (int j = 1; j <= nums[i]; j++) {
-//                 if (dfs(nums, i+j, memo)) {
-//                     memo[i] = true;
-//                     return memo[i];
-//                 }
-//             }
-//             memo[i] = false;
-//         }
-//         return memo[i];
-//     }
+        for (int i = nums.length-2; i >= 0; i--) {
+            if (reachable-i <= nums[i]) {
+                reachable = i;
+            }
+            
+        }
+        return reachable == 0;
+    }
+
 }
