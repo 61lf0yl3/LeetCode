dac5311f86007746aba900c54c94ad5f180940e8
61lf0yl3 <54092529+61lf0yl3@users.noreply.github.com>
2023-04-19 01:06:53 -0700

    Time: 144 ms (21.12%), Space: 42.7 MB (86.52%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,22 +1,32 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        return dfs(nums, 0, new Boolean[nums.length]);
-    }
-    
-    private boolean dfs(int[] nums, int i, Boolean[] memo) {
-        if (i >= nums.length-1) {
-            return true;
-        }
-        
-        if (memo[i] == null) {
+        boolean[] dp = new boolean[nums.length];
+        dp[nums.length-1] = true;
+        for (int i = nums.length-2; i >= 0; i--) {
             for (int j = 1; j <= nums[i]; j++) {
-                if (dfs(nums, i+j, memo)) {
-                    memo[i] = true;
-                    return memo[i];
+                if (dp[i+j]) {
+                    dp[i] = true;
+                    break;
                 }
             }
-            memo[i] = false;
         }
-        return memo[i];
+        return dp[0];
     }
+    
+//     private boolean dfs(int[] nums, int i, Boolean[] memo) {
+//         if (i >= nums.length-1) {
+//             return true;
+//         }
+        
+//         if (memo[i] == null) {
+//             for (int j = 1; j <= nums[i]; j++) {
+//                 if (dfs(nums, i+j, memo)) {
+//                     memo[i] = true;
+//                     return memo[i];
+//                 }
+//             }
+//             memo[i] = false;
+//         }
+//         return memo[i];
+//     }
 }
