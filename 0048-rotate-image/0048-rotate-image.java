cda2740ddd3aa2c22180f208e504d4c4f47a3951
61lf0yl3 <54092529+61lf0yl3@users.noreply.github.com>
2023-04-20 23:10:43 -0700

    Time: 0 ms (100.00%), Space: 42.5 MB (49.68%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,40 +1,54 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        // transpose(matrix);
-        // reverse(matrix);
-        int left = 0;
-        int right = matrix.length-1;
+        transpose(matrix);
+        reverse(matrix);
+//         int left = 0;
+//         int right = matrix.length-1;
         
-        while (left < right) {
-            for (int i = 0; i < right-left; i++) {
-                int top = left;
-                int bottom = right;
-                // safe the top left
-                int topLeft = matrix[top][left+i];
+//         while (left < right) {
+//             for (int i = 0; i < right-left; i++) {
+//                 int top = left;
+//                 int bottom = right;
+//                 // safe the top left
+//                 int topLeft = matrix[top][left+i];
                 
-                // overwrite top-left with bottom-left
-                matrix[top][left+i] = matrix[bottom-i][left];
+//                 // overwrite top-left with bottom-left
+//                 matrix[top][left+i] = matrix[bottom-i][left];
                 
-                // overwrite bottom-left with bottom-right
-                matrix[bottom-i][left] = matrix[bottom][right-i];
+//                 // overwrite bottom-left with bottom-right
+//                 matrix[bottom-i][left] = matrix[bottom][right-i];
                 
-                // overwrite bottom-right with top-right
-                matrix[bottom][right-i] = matrix[top+i][right];
+//                 // overwrite bottom-right with top-right
+//                 matrix[bottom][right-i] = matrix[top+i][right];
                 
-                // overwrite top-right with top-left;
-                matrix[top+i][right] = topLeft;
+//                 // overwrite top-right with top-left;
+//                 matrix[top+i][right] = topLeft;
+//             }
+//             left++;
+//             right--;
+//         }
+    }
+    
+    private void transpose(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
-            left++;
-            right--;
         }
     }
     
-//     private void rotate(int[][] matrix) {
-        
-//     }
-    
-//     private void reverse(int[][] matrix) {
-        
-//     }
+    private void reverse(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n/2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-1-j];
+                matrix[i][n-1-j] = temp;
+            }
+        }
+    }
     
 }
