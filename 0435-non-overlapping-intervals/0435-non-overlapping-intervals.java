c3f214f96d3b4ef608cb4ecb0017949a262ad15b
61lf0yl3 <54092529+61lf0yl3@users.noreply.github.com>
2023-04-27 15:39:16 -0700

    Time: 70 ms (36.52%), Space: 101.8 MB (8.22%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,24 +1,40 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        Arrays.sort(intervals, (int[] a, int[] b) -> {
-            return a[0]-b[0];
-        });
-        
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         int res = 0;
-        int endNew = intervals[0][1];
-        
+        int end = intervals[0][1];
         for (int i = 1; i < intervals.length; i++) {
-            int[] interval = intervals[i];
-            
-            // if doesn't overla[]
-            if ( interval[0] < endNew ) {
-                endNew = Math.min(endNew, interval[1]);
+            if (intervals[i][0] < end) {
                 res++;
+                end = Math.min(end, intervals[i][1]);
             } else {
-                endNew = interval[1];
+                end = intervals[i][1];
             }
         }
-        
         return res;
     }
 }
+
+
+
+//         Arrays.sort(intervals, (int[] a, int[] b) -> {
+//             return a[0]-b[0];
+//         });
+        
+//         int res = 0;
+//         int endNew = intervals[0][1];
+        
+//         for (int i = 1; i < intervals.length; i++) {
+//             int[] interval = intervals[i];
+            
+//             // if prev and curr overlap pick end smaller, and one of them gonna deleted
+//             if ( interval[0] < endNew ) {
+//                 endNew = Math.min(endNew, interval[1]);
+//                 res++;
+//             } else {
+//             // update the end
+//                 endNew = interval[1];
+//             }
+//         }
+        
+//         return res;
