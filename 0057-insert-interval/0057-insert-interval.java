cbf1160b5bce0bb99b18a29083b5a035ee86d0db
61lf0yl3 <54092529+61lf0yl3@users.noreply.github.com>
2023-04-27 15:21:27 -0700

    Time: 1 ms (97.70%), Space: 45.1 MB (16.35%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -4,21 +4,25 @@ class Solution {
         
         int startNew = newInterval[0];
         int endNew = newInterval[1];
+        
+        
         for (int i = 0; i < intervals.length; i++) {
             int[] interval = intervals[i];
-            // new interval ends before start of current interval
-            if ( endNew < interval[0]) {
+            
+            if (endNew < interval[0]) {
                 list.add(new int[]{startNew, endNew});
+                
                 for (int j = i; j < intervals.length; j++) {
                     list.add(intervals[j]);
                 }
+                
                 int[][] res = new int[list.size()][2];
-                for (int j = 0; j < res.length; j++) {
+                for (int j = 0; j < list.size(); j++) {
                     res[j] = list.get(j);
-                }
+                };
                 return res;
-            // new interval start after end of current interval
-            } else if (startNew > interval[1]) {
+                
+            } else if (interval[1] < startNew){
                 list.add(interval);
             } else {
                 startNew = Math.min(startNew, interval[0]);
@@ -27,9 +31,47 @@ class Solution {
         }
         list.add(new int[]{startNew, endNew});
         int[][] res = new int[list.size()][2];
-        for (int i = 0; i < res.length; i++) {
+        for (int i = 0; i < list.size(); i++) {
             res[i] = list.get(i);
-        }
+        };
         return res;
     }
+    
 }
+
+
+// class Solution {
+//     public int[][] insert(int[][] intervals, int[] newInterval) {
+//         List<int[]> list = new ArrayList<>();
+        
+//         int startNew = newInterval[0];
+//         int endNew = newInterval[1];
+//         for (int i = 0; i < intervals.length; i++) {
+//             int[] interval = intervals[i];
+//             // new interval ends before start of current interval
+//             if ( endNew < interval[0]) {
+//                 list.add(new int[]{startNew, endNew});
+//                 for (int j = i; j < intervals.length; j++) {
+//                     list.add(intervals[j]);
+//                 }
+//                 int[][] res = new int[list.size()][2];
+//                 for (int j = 0; j < res.length; j++) {
+//                     res[j] = list.get(j);
+//                 }
+//                 return res;
+//             // new interval start after end of current interval
+//             } else if (startNew > interval[1]) {
+//                 list.add(interval);
+//             } else {
+//                 startNew = Math.min(startNew, interval[0]);
+//                 endNew = Math.max(endNew, interval[1]);
+//             }
+//         }
+//         list.add(new int[]{startNew, endNew});
+//         int[][] res = new int[list.size()][2];
+//         for (int i = 0; i < res.length; i++) {
+//             res[i] = list.get(i);
+//         }
+//         return res;
+//     }
+// }
