32e29baba323e23bdabed2fed1055d8bbf8d4dd2
61lf0yl3 <54092529+61lf0yl3@users.noreply.github.com>
2023-05-24 04:42:23 -0700

    Time: 6 ms (88.86%), Space: 41.9 MB (97.68%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,49 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int[] chars = new int[26];
+        for (char c : s1.toCharArray()) {
+            chars[c-'a']++;
+        }
+        int left = 0;
+        int right = 0;
+        while (right < s2.length()) {
+            int c = s2.charAt(right);
+            if (chars[c-'a'] == 0) {
+                chars[s2.charAt(left)-'a']++;
+                left++;
+            } else {
+                chars[c-'a']--;
+                right++;
+                if (right-left == s1.length()) {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+}
+
+// public boolean checkInclusion(String s1, String s2) {
+//         int[] chars = new int[26];
+//         for (char c : s1.toCharArray()) {
+//             chars[c-'a']++;
+//         }
+//         int left = 0;
+//         //int right = 0;
+//         for (int right = 0; right < s2.length(); right++) {
+//             char c = s2.charAt(right);
+//             if (chars[c-'a']==0) {
+//                 chars[s2.charAt(left)-'a']++;
+//                 left++;
+//                 right--;
+//                 continue;
+//             } else {
+//                 chars[c-'a']--;
+//                 if (right-left+1==s1.length()) {
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
