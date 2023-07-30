02a085a98b271c5abc968a21ae7d7d7a3777d652
61lf0yl3 <54092529+61lf0yl3@users.noreply.github.com>
2023-07-29 17:40:18 -0700

    Time: 28 ms (100.00%), Space: 54.7 MB (55.72%) - LeetHub

---
+++ 0359-logger-rate-limiter/0359-logger-rate-limiter.java
@@ -0,0 +1,23 @@
+class Logger {
+    Map<String, Integer> log;
+    public Logger() {
+        log = new HashMap<>();
+    }
+    
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        Map<String, Integer> log1 = log;
+        if (log.containsKey(message)) {
+            if (log.get(message)+10 > timestamp) {
+                return false;
+            }
+        }
+        log.put(message, timestamp);
+        return true;
+    }
+}
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger obj = new Logger();
+ * boolean param_1 = obj.shouldPrintMessage(timestamp,message);
+ */
